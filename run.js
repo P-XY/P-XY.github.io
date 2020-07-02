87386dcef5e25dfcc57c5fc1f8866964b81f642d
xiaoyupang <xiaoyu_pang@qq.com>
2020-07-02 11:39:57 +0800

    x

--- run.js
+++ run.js
@@ -1,7 +1,6 @@
 
 var exec = require('child_process').exec
 
-/* promise 写法
 let promise = Promise.resolve();
 promise
 .then(
@@ -10,20 +9,4 @@ promise
     ()=>exec('git commit -m"x"',console.log("commit success")) )
 .then(
     ()=>exec( 'git push',console.log('push success') ) )
-*/
 
-//async/await 写法
-
-function exe(str){
-    return Promise.resolve().then( ()=>exec(str))
-}
-
-async function run(){
-    let add = await exe('git add .')
-    let commit = await exe( 'git commit -m"z"') 
-    let push = await exe( 'git push') 
-    return 
-}
-
-run()
-.catch( e => console.log( 'error ' + e.message))
